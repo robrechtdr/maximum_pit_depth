538074d074c62600583f8970269145c5eccd29c4
Robrecht De Rouck <de.rouck.robrecht@gmail.com>
2015-01-20 21:20:10 +0100

    Document functions and rename file.

--- maximum_pit_depth.py
+++ maximum_pit_depth.py
@@ -1,6 +1,8 @@
 # tc: O(n**4)
 # sc: O(n)
-def get_deepest_array_pit(A):
+def get_maximum_pit_depth(A):
+    """Get the depth of the deepest pit.
+    """
     depths = _get_pit_depths(A)
     return max(depths)
 
@@ -8,6 +10,8 @@ def get_deepest_array_pit(A):
 # tc: O(n**4)
 # sc: O(n)
 def _get_pit_depths(A):
+    """Get the depths of all occuring pits in the array.
+    """
     depths = []
     for i, n in enumerate(A):
         for j, o in enumerate(A):
@@ -23,6 +27,8 @@ def _get_pit_depths(A):
 # tc: O(n)
 # sc: O(1)
 def _is_pit(A, p, q, r):
+    """Check whether the triplet defined by p, q and r in the array is a pit.
+    """
     if _is_consecutively_ch("decr", A, p, q) and _is_consecutively_ch("incr", A, q, r):
         return True
     else:
@@ -31,13 +37,17 @@ def _is_pit(A, p, q, r):
 
 # tc: O(n)
 # sc: O(1)
-def _is_consecutively_ch(change, A, a, b):
+def _is_consecutively_ch(direction, A, a, b):
+    """
+    Check whether the values in the array from item with index a to item with
+    index b are consecutively changing in a given direction.
+    """
     first = A[a]
     for n in A[a:b + 1]:
-        if change == "incr":
+        if direction == "incr":
             if first > n:
                 return False
-        elif change == "decr":
+        elif direction == "decr":
             if first < n:
                 return False
         first = n
@@ -49,9 +59,11 @@ def _is_consecutively_ch(change, A, a, b):
 # tc: O(1)
 # sc: O(1)
 def _get_depth(pit):
+    """Get the depth of a given pit.
+    """
     return min(pit[0] - pit[1], pit[2] - pit[1])
 
 
 A = [0, 1, 3, -2, 0, 1, 0, -3, 2, 3]
-print get_deepest_array_pit(A)
+print get_maximum_pit_depth(A)
 #4
