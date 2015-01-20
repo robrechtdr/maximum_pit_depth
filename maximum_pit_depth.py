ec15b5228b2e37eaf770cc6225884a9ff4ecb672
Robrecht De Rouck <de.rouck.robrecht@gmail.com>
2015-01-20 22:51:54 +0100

    Add test case without pits

--- maximum_pit_depth.py
+++ maximum_pit_depth.py
@@ -4,7 +4,11 @@ def get_maximum_pit_depth(A):
     """Get the depth of the deepest pit.
     """
     depths = _get_pit_depths(A)
-    return max(depths)
+    print depths
+    if not depths:
+        return -1
+    else:
+        return max(depths)
 
 
 # tc: O(n**4)
@@ -19,6 +23,7 @@ def _get_pit_depths(A):
                 if i < j < k:
                     triplet = (n, o, p)
                     if _is_pit(A, i, j, k):
+                        print i, j, k
                         depth = _get_depth(triplet)
                         depths.append(depth)
     return depths
@@ -29,7 +34,8 @@ def _get_pit_depths(A):
 def _is_pit(A, p, q, r):
     """Check whether the triplet defined by p, q and r in the array is a pit.
     """
-    if _is_consecutively_ch("decr", A, p, q) and _is_consecutively_ch("incr", A, q, r):
+    if _is_consec_ch_in_direction("decr", A, p, q) and \
+        _is_consec_ch_in_direction("incr", A, q, r):
         return True
     else:
         return False
@@ -37,23 +43,21 @@ def _is_pit(A, p, q, r):
 
 # tc: O(n)
 # sc: O(1)
-def _is_consecutively_ch(direction, A, a, b):
+def _is_consec_ch_in_direction(direction, A, a, b):
     """
     Check whether the values in the array from item with index a to item with
     index b are consecutively changing in a given direction.
     """
-    first = A[a]
-    for n in A[a:b + 1]:
+    m = A[a]
+    for n in A[a + 1:b + 1]:
         if direction == "incr":
-            if first > n:
+            if n <= m:
                 return False
         elif direction == "decr":
-            if first < n:
+            if n >= m:
                 return False
-        first = n
-
-    else:
-        return True
+        m = n
+    return True
 
 
 # tc: O(1)
--- tests.py
+++ tests.py
@@ -1,18 +1,21 @@
 import pytest
 import unittest
 from maximum_pit_depth import (get_maximum_pit_depth, _get_pit_depths,
-                               _is_pit, _is_consecutively_ch, _get_depth)
+                               _is_pit, _is_consec_ch_in_direction, _get_depth)
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.ARR0 = [0, 1, 3, -2, 0, 1, 0, -3, 2, 3]
+        # No pits
+        self.ARR1 = [2, 2, 4, 9]
 
     def tearDown(self):
         pass
 
     def test_get_maximum_pit_depth(self):
         assert get_maximum_pit_depth(self.ARR0) == 4
+        assert get_maximum_pit_depth(self.ARR1) == -1
 
 
     def test_get_pit_depths(self):
@@ -26,12 +29,14 @@ class MyTestCase(unittest.TestCase):
         assert _is_pit(self.ARR0, 5, 7, 8) == True
 
 
-    def test_is_consecutively_ch(self):
-        assert _is_consecutively_ch("incr", self.ARR0, 0, 2) == True
+    def test_is_consec_ch_in_direction(self):
+        assert _is_consec_ch_in_direction("incr", self.ARR0, 0, 2) == True
+        assert _is_consec_ch_in_direction("decr", [2, 2], 0, 1) == False
+        assert _is_consec_ch_in_direction("incr", [2, 4], 0, 1) == True
+
 
     def test_get_depth(self):
         assert _get_depth((3, -2, 0)) == 2
-
 
 
 if __name__ == "__main__":
